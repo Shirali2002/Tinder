9ea27359f23ace94eafecfeb42e4ea26f78f6d03
Shirali2002 <shiralialihummatov@gmail.com>
2022-10-25 17:11:55 +0400

    changes

--- src/main/java/com/abbtech/domain/User.java
+++ src/main/java/com/abbtech/domain/User.java
@@ -1,32 +1,18 @@
 package com.abbtech.domain;
 
-import com.abbtech.dto.PpLink;
-import com.abbtech.exceptions.NoSuchUserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.Arrays;
+import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class User {
+
   private int id;
   private String username;
-  private PpLink link;
-
   private String password;
-
-  public User(int id, String username,String password, int linkId) {
-    this.id = id;
-    this.username = username;
-    this.password = password;
-    this.link = getPpLink(linkId);
-  }
-
-  private PpLink getPpLink(int linkId) {
-    return Arrays.stream(PpLink.values())
-        .filter(p -> p.getId() == linkId)
-        .findFirst()
-        .orElseThrow(NoSuchUserException::new);
-  }
+  private LocalDateTime lastSeen;
+  private String link;
 
 }
---
+++ src/main/java/com/abbtech/templateEngine/TemplateEngine.java
@@ -0,0 +1,42 @@
+package com.abbtech.templateEngine;
+
+import freemarker.template.Configuration;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class TemplateEngine {
+
+    private final Configuration config;
+
+    public TemplateEngine() throws IOException, URISyntaxException {
+        this.config = new Configuration(Configuration.VERSION_2_3_28) {{
+            String path =
+                    Paths.get(TemplateEngine.class.getResource("/WEB-INF").toURI())
+                            .toFile().getAbsolutePath();
+            setDirectoryForTemplateLoading(new File(path));
+            setDefaultEncoding(String.valueOf(StandardCharsets.UTF_8));
+            setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+            setLogTemplateExceptions(false);
+            setWrapUncheckedExceptions(true);
+        }};
+    }
+
+    public void render(String templateFile, Map<String, Object> data, HttpServletResponse rs)
+            throws IOException {
+        try {
+            rs.setCharacterEncoding(String.valueOf(StandardCharsets.UTF_8));
+            config.getTemplate(templateFile).process(data, rs.getWriter());
+        } catch (TemplateException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
