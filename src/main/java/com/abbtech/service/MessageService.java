a1fbcf9b5694610eb154e1a476f2fb41ddaa1540
vusalahasanova01 <vusalahasanova423@gmail.com>
2022-10-26 02:22:59 +0400

    changes

--- src/main/java/com/abbtech/service/MessageService.java
+++ src/main/java/com/abbtech/service/MessageService.java
@@ -1,4 +1,27 @@
 package com.abbtech.service;
 
+import com.abbtech.domain.Message;
+import com.abbtech.repository.MessageRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class MessageService {
+  MessageRepository messageRepo = new MessageRepository();
+
+  public List<Message> getAll() {
+    return messageRepo.getAll();
+  }
+
+  public Optional<Message> getById(int id) {
+    return messageRepo.getById(id);
+  }
+
+  public boolean add(Message value) {
+    return messageRepo.add(value);
+  }
+
+  public boolean delete(int id) {
+    return messageRepo.delete(id);
+  }
 }
